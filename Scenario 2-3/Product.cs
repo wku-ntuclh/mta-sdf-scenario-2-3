63e8d3b9ad5bc38a7ab96b942be5a7b38f9ade27
William Ku <kucheech@gmail.com>
2018-09-08 23:10:08 +0800

    Add project files.

---
+++ Scenario 2-3/Product.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Scenario_2_3
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public override string ToString()
+        {
+            return "Name: " + Name + "\t Price: $" + Price.ToString("0.##");
+        }
+    }
+}
---
+++ Scenario 2-3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Scenario_2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Product p = new Product
+            {
+                Name = "IPhone",
+                Price = 1299
+            };
+            Console.WriteLine(p.ToString());
+        }
+    }
+}
